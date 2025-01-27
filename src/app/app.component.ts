8d12f7943a14137d30f48d3c5751cc8d90454cde
Ricardo Gimenez <ricardo.gimenez096@gmail.com>
2025-01-27 17:24:52 -0300

    feat: Add work environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TableModule } from 'primeng/table';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TableModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'joaju-calls';
+
 }

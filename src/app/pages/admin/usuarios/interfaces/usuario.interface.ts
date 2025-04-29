d6941c33f3c7c7a87ebd92e536367c7b0b700b53
marcelo-solvan <marcelo.morinigo@solvan.com.py>
2025-04-29 16:06:30 -0300

    Se agregó componente para asociar teams a usuarios

--- src/app/pages/admin/usuarios/interfaces/usuario.interface.ts
+++ src/app/pages/admin/usuarios/interfaces/usuario.interface.ts
@@ -1,51 +1,67 @@
 export interface Usuario {
-    id_usuario: string;
-    n_usuario: string;
-    team: string;
-    nro_exten: string;
-    nro_agente: string;
-    nom: string;
-    ape: string;
-    acd_predef: string;
-    n_perfil: string;
-    id_perfil: string;
-    activo: string;
-    id_team: string;
+  id_usuario: string;
+  n_usuario: string;
+  team: string;
+  nro_exten: string;
+  nro_agente: string;
+  nom: string;
+  ape: string;
+  acd_predef: string;
+  n_perfil: string;
+  id_perfil: string;
+  activo: string;
+  id_team: string;
 }
-  
+
 export interface UsuarioResponse {
-    err_code: string;
-    mensaje: {
-      [key: string]: Usuario;
-    };
+  err_code: string;
+  mensaje: {
+    [key: string]: Usuario;
+  };
 }
 
 export interface createUsuario {
-    service: string;
-    usuario: string;
-    nombre: string;
-    apellido: string;
-    crea_pass: string;
-    crea_pass2: string;
-    acd_predef: string;
+  service: string;
+  usuario: string;
+  nombre: string;
+  apellido: string;
+  crea_pass: string;
+  crea_pass2: string;
+  acd_predef: string;
 }
 
 export interface CreateUsuarioResponse {
-    err_code: string;
-    mensaje: string;
-    last_id?: string;
+  err_code: string;
+  mensaje: string;
+  last_id?: string;
 }
 
 export interface updateUsuario {
-    service: string;
-    id_usuario: string;
-    usuario: string;
-    nombre: string;
-    apellido: string;
-    crea_pass: string;
-    crea_pass2: string;
-    acd_predef: string;
-    id_perfil: string;
-    agente: string;
-    nro_exten: string;
+  service: string;
+  id_usuario: string;
+  usuario: string;
+  nombre: string;
+  apellido: string;
+  crea_pass: string;
+  crea_pass2: string;
+  acd_predef: string;
+  id_perfil: string;
+  agente: string;
+  nro_exten: string;
+}
+
+export interface TeamInfo {
+  id_team: string;
+  n_team: string;
+}
+
+export interface TeamAclResponse {
+  err_code: string;
+  mensaje: {
+    teams: {
+      [key: string]: TeamInfo;
+    };
+    acl: string[];
+    teams_colas: string[];
+  };
 }
--- src/app/pages/admin/usuarios/services/usuarios.service.ts
+++ src/app/pages/admin/usuarios/services/usuarios.service.ts
@@ -1,62 +1,82 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core"
-import { map, Observable } from "rxjs";
-import { environment } from "../../../../environments/environtment";
-import { UsuarioResponse } from "../interfaces/usuario.interface";
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { environment } from '../../../../environments/environtment';
+import { TeamAclResponse, UsuarioResponse } from '../interfaces/usuario.interface';
 
 @Injectable({
-    providedIn: "root"
+  providedIn: 'root',
 })
 export class UsuariosService {
-    apiUrl: string = environment.apiUrl;
-    constructor(private http: HttpClient) {}
+  apiUrl: string = environment.apiUrl;
+  constructor(private http: HttpClient) {}
 
-    getUsuarios(): Observable<UsuarioResponse> {
-        return this.http.get<UsuarioResponse>(`${this.apiUrl}/usuarios?service=lista_usuarios`);
-    }
-    
-    createUsuario(formData: FormData): Observable<any> {
-        return this.http.post(`${this.apiUrl}/usuarios`, formData, {
-          responseType: 'text'
-        }).pipe(
-          map((response: string) => {
-            console.log('Respuesta original:', response);
-      
-            if (response === 'Usuario ya existe') {
+  getUsuarios(): Observable<UsuarioResponse> {
+    return this.http.get<UsuarioResponse>(
+      `${this.apiUrl}/usuarios?service=lista_usuarios`
+    );
+  }
+
+  createUsuario(formData: FormData): Observable<any> {
+    return this.http
+      .post(`${this.apiUrl}/usuarios`, formData, {
+        responseType: 'text',
+      })
+      .pipe(
+        map((response: string) => {
+          console.log('Respuesta original:', response);
+
+          if (response === 'Usuario ya existe') {
+            return {
+              err_code: 'error',
+              mensaje: 'Usuario ya existe',
+            };
+          }
+
+          try {
+            const lastIdMatch = response.match(/\[last_id\] => (\d+)/);
+            const lastId = lastIdMatch ? lastIdMatch[1] : null;
+
+            if (lastId) {
               return {
-                err_code: "error",
-                mensaje: "Usuario ya existe"
+                lastId,
+                err_code: '200',
+                mensaje: 'Usuario creado exitosamente',
+              };
+            } else {
+              return {
+                err_code: 'error',
+                mensaje: response,
               };
             }
-      
-            try {
-              const lastIdMatch = response.match(/\[last_id\] => (\d+)/);
-              const lastId = lastIdMatch ? lastIdMatch[1] : null;
-      
-              if (lastId) {
-                return {
-                  lastId,
-                  err_code: "200",
-                  mensaje: "Usuario creado exitosamente"
-                };
-              } else {
-                return {
-                  err_code: "error",
-                  mensaje: response
-                };
-              }
-            } catch (error) {
-              console.error('Error procesando respuesta:', error);
-              return {
-                err_code: "error",
-                mensaje: response
-              };
-            }
-          })
-        );
-    }
+          } catch (error) {
+            console.error('Error procesando respuesta:', error);
+            return {
+              err_code: 'error',
+              mensaje: response,
+            };
+          }
+        })
+      );
+  }
 
-    updateUsuario(formData: FormData): Observable<any>{
-        return this.http.post(`${this.apiUrl}/usuarios`, formData)
-    }
+  updateUsuario(formData: FormData): Observable<any> {
+    return this.http.post(`${this.apiUrl}/usuarios`, formData);
+  }
+
+  listAclSup(usuario: string): Observable<TeamAclResponse> {
+    const params = new HttpParams()
+        .set('service', 'list_acl_sup')
+        .set('usuario', usuario);
+
+    return this.http.get<TeamAclResponse>(`${this.apiUrl}/usuarios`, { params });
+  }
+
+  addUsuarioTeam(formData: FormData): Observable<any> {
+    return this.http.post(`${this.apiUrl}/usuarios`, formData);
+  }
+
+  delUsuarioTeam(formData: FormData): Observable<any> {
+    return this.http.post(`${this.apiUrl}/usuarios`, formData);
+  }
 }
